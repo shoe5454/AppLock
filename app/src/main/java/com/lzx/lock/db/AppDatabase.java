6efc278e31d6752d99e4a521dd891e9c67869b9b
Shu <shu@kiwadigital.com>
2020-11-02 00:28:10 +1300

    .

--- app/src/main/java/com/lzx/lock/db/AppDatabase.java
+++ app/src/main/java/com/lzx/lock/db/AppDatabase.java
@@ -7,6 +7,7 @@ import android.database.sqlite.SQLiteDatabase;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.room.migration.Migration;
 import androidx.sqlite.db.SupportSQLiteDatabase;
 
 import com.lzx.lock.R;
@@ -15,9 +16,50 @@ import com.lzx.lock.db.entities.Answer;
 import com.lzx.lock.db.entities.AnswerSubtype;
 import com.lzx.lock.db.entities.AnswerType;
 
-@Database(entities = {Answer.class}, version = 1)
+@Database(entities = {Answer.class}, version = 2)
 public abstract class AppDatabase extends RoomDatabase {
     public abstract AnswerDao answerDao();
+
+    private static void initialPopulate(SupportSQLiteDatabase db) {
+        insert(db, AnswerType.THING_IDENTIFICATION, AnswerSubtype.ANIMAL, R.drawable.question_bird, "A bird", false);
+        insert(db, AnswerType.THING_IDENTIFICATION, AnswerSubtype.ANIMAL, R.drawable.question_cat, "A cat", false);
+        insert(db, AnswerType.THING_IDENTIFICATION, AnswerSubtype.ANIMAL, R.drawable.question_cow, "A cow", false);
+        insert(db, AnswerType.THING_IDENTIFICATION, AnswerSubtype.ANIMAL, R.drawable.question_dog, "A dog", false);
+        insert(db, AnswerType.THING_IDENTIFICATION, AnswerSubtype.ANIMAL, R.drawable.question_duck, "A duck", false);
+        insert(db, AnswerType.THING_IDENTIFICATION, AnswerSubtype.ANIMAL, R.drawable.question_elephant, "An elephant", false);
+        insert(db, AnswerType.THING_IDENTIFICATION, AnswerSubtype.ANIMAL, R.drawable.question_fish, "A fish", false);
+        insert(db, AnswerType.THING_IDENTIFICATION, AnswerSubtype.ANIMAL, R.drawable.question_horse, "A horse", false);
+        insert(db, AnswerType.THING_IDENTIFICATION, AnswerSubtype.ANIMAL, R.drawable.question_lion, "A lion", false);
+        insert(db, AnswerType.THING_IDENTIFICATION, AnswerSubtype.ANIMAL, R.drawable.question_owl, "An owl", false);
+        insert(db, AnswerType.THING_IDENTIFICATION, AnswerSubtype.ANIMAL, R.drawable.question_pig, "A pig", false);
+        insert(db, AnswerType.THING_IDENTIFICATION, AnswerSubtype.ANIMAL, R.drawable.question_shark, "A shark", false);
+        insert(db, AnswerType.THING_IDENTIFICATION, AnswerSubtype.ANIMAL, R.drawable.question_whale, "A whale", false);
+        insert(db, AnswerType.THING_IDENTIFICATION, AnswerSubtype.VEHICLE, R.drawable.question_airplane, "An airplane", false);
+        insert(db, AnswerType.THING_IDENTIFICATION, AnswerSubtype.VEHICLE, R.drawable.question_bicycle, "A bicycle", false);
+        insert(db, AnswerType.THING_IDENTIFICATION, AnswerSubtype.VEHICLE, R.drawable.question_boat, "A boat", false);
+        insert(db, AnswerType.THING_IDENTIFICATION, AnswerSubtype.VEHICLE, R.drawable.question_bus, "A bus", false);
+        insert(db, AnswerType.THING_IDENTIFICATION, AnswerSubtype.VEHICLE, R.drawable.question_car, "A car", false);
+        insert(db, AnswerType.THING_IDENTIFICATION, AnswerSubtype.VEHICLE, R.drawable.question_helicopter, "A helicopter", false);
+        insert(db, AnswerType.THING_IDENTIFICATION, AnswerSubtype.VEHICLE, R.drawable.question_rocket, "A rocket", false);
+        insert(db, AnswerType.THING_IDENTIFICATION, AnswerSubtype.VEHICLE, R.drawable.question_train, "A train", false);
+        insert(db, AnswerType.THING_IDENTIFICATION, AnswerSubtype.VEHICLE, R.drawable.question_truck, "A truck", false);
+        insert(db, AnswerType.THING_IDENTIFICATION, AnswerSubtype.FOOD, R.drawable.question_bread, "Bread", false);
+        insert(db, AnswerType.THING_IDENTIFICATION, AnswerSubtype.FOOD, R.drawable.question_cheese, "Cheese", false);
+        insert(db, AnswerType.THING_IDENTIFICATION, AnswerSubtype.FOOD, R.drawable.question_egg, "An egg", false);
+        insert(db, AnswerType.THING_IDENTIFICATION, AnswerSubtype.FOOD, R.drawable.question_ice_cream, "Ice cream", false);
+        insert(db, AnswerType.THING_IDENTIFICATION, AnswerSubtype.FOOD, R.drawable.question_milk, "Milk", false);
+        insert(db, AnswerType.THING_IDENTIFICATION, AnswerSubtype.FOOD, R.drawable.question_pizza, "A pizza", false);
+    }
+
+    static final Migration MIGRATION_1_2 = new Migration(1, 2) {
+        @Override
+        public void migrate(SupportSQLiteDatabase db) {
+            insert(db, AnswerType.THING_IDENTIFICATION, AnswerSubtype.FOOD, R.drawable.question_cake, "A cake", false);
+            // banana
+            // family relationships
+            // bee
+        }
+    };
 
     private static void insert(SupportSQLiteDatabase db, AnswerType type, AnswerSubtype subtype, int imageResId, String text, boolean plural/*, AnswerQuantifier quantifier*/) {
         ContentValues cv = new ContentValues();
@@ -36,40 +78,12 @@ public abstract class AppDatabase extends RoomDatabase {
     public static AppDatabase populate(Context applicationContext) {
         RoomDatabase.Callback rdc = new RoomDatabase.Callback() {
             public void onCreate (SupportSQLiteDatabase db) {
-                insert(db, AnswerType.THING_IDENTIFICATION, AnswerSubtype.ANIMAL, R.drawable.question_bird, "A bird", false);
-                insert(db, AnswerType.THING_IDENTIFICATION, AnswerSubtype.ANIMAL, R.drawable.question_cat, "A cat", false);
-                insert(db, AnswerType.THING_IDENTIFICATION, AnswerSubtype.ANIMAL, R.drawable.question_cow, "A cow", false);
-                insert(db, AnswerType.THING_IDENTIFICATION, AnswerSubtype.ANIMAL, R.drawable.question_dog, "A dog", false);
-                insert(db, AnswerType.THING_IDENTIFICATION, AnswerSubtype.ANIMAL, R.drawable.question_duck, "A duck", false);
-                insert(db, AnswerType.THING_IDENTIFICATION, AnswerSubtype.ANIMAL, R.drawable.question_elephant, "An elephant", false);
-                insert(db, AnswerType.THING_IDENTIFICATION, AnswerSubtype.ANIMAL, R.drawable.question_fish, "A fish", false);
-                insert(db, AnswerType.THING_IDENTIFICATION, AnswerSubtype.ANIMAL, R.drawable.question_horse, "A horse", false);
-                insert(db, AnswerType.THING_IDENTIFICATION, AnswerSubtype.ANIMAL, R.drawable.question_lion, "A lion", false);
-                insert(db, AnswerType.THING_IDENTIFICATION, AnswerSubtype.ANIMAL, R.drawable.question_owl, "An owl", false);
-                insert(db, AnswerType.THING_IDENTIFICATION, AnswerSubtype.ANIMAL, R.drawable.question_pig, "A pig", false);
-                insert(db, AnswerType.THING_IDENTIFICATION, AnswerSubtype.ANIMAL, R.drawable.question_shark, "A shark", false);
-                insert(db, AnswerType.THING_IDENTIFICATION, AnswerSubtype.ANIMAL, R.drawable.question_whale, "A whale", false);
-                insert(db, AnswerType.THING_IDENTIFICATION, AnswerSubtype.VEHICLE, R.drawable.question_airplane, "An airplane", false);
-                insert(db, AnswerType.THING_IDENTIFICATION, AnswerSubtype.VEHICLE, R.drawable.question_bicycle, "A bicycle", false);
-                insert(db, AnswerType.THING_IDENTIFICATION, AnswerSubtype.VEHICLE, R.drawable.question_boat, "A boat", false);
-                insert(db, AnswerType.THING_IDENTIFICATION, AnswerSubtype.VEHICLE, R.drawable.question_bus, "A bus", false);
-                insert(db, AnswerType.THING_IDENTIFICATION, AnswerSubtype.VEHICLE, R.drawable.question_car, "A car", false);
-                insert(db, AnswerType.THING_IDENTIFICATION, AnswerSubtype.VEHICLE, R.drawable.question_helicopter, "A helicopter", false);
-                insert(db, AnswerType.THING_IDENTIFICATION, AnswerSubtype.VEHICLE, R.drawable.question_rocket, "A rocket", false);
-                insert(db, AnswerType.THING_IDENTIFICATION, AnswerSubtype.VEHICLE, R.drawable.question_train, "A train", false);
-                insert(db, AnswerType.THING_IDENTIFICATION, AnswerSubtype.VEHICLE, R.drawable.question_truck, "A truck", false);
-                insert(db, AnswerType.THING_IDENTIFICATION, AnswerSubtype.FOOD, R.drawable.question_bread, "Bread", false);
-                insert(db, AnswerType.THING_IDENTIFICATION, AnswerSubtype.FOOD, R.drawable.question_cheese, "Cheese", false);
-                insert(db, AnswerType.THING_IDENTIFICATION, AnswerSubtype.FOOD, R.drawable.question_egg, "An egg", false);
-                insert(db, AnswerType.THING_IDENTIFICATION, AnswerSubtype.FOOD, R.drawable.question_ice_cream, "Ice cream", false);
-                insert(db, AnswerType.THING_IDENTIFICATION, AnswerSubtype.FOOD, R.drawable.question_milk, "Milk", false);
-                insert(db, AnswerType.THING_IDENTIFICATION, AnswerSubtype.FOOD, R.drawable.question_pizza, "A pizza", false);
-                // banana
-                // family relationships
+                initialPopulate(db);
             }
         };
         return Room.databaseBuilder(applicationContext, AppDatabase.class, "reading-lock")
                 .addCallback(rdc)
+                .addMigrations(MIGRATION_1_2)
                 .build();
     }
 }
